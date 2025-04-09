36a79ee0f3bf1419775aea9a43565e8d142f2969
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-04-09 06:33:22 -0500

    feat: refactor app.js to use CommonJS syntax and add EJS view for index

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import "dotenv/config";
-import express from "express";
-import pkg from "pg";
-
+require("dotenv/config");
+const express = require("express");
 const app = express();
-const { Pool } = pkg;
+const { Pool } = require("pg");
+const path = require("node:path");
 
 const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index", { message: "Hello World!" });
 });
 
 app.listen(process.env.PORT, (req, res) => {
